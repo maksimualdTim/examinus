3403faf908deafed6be06b3a7629669b4f3ae484
maksimualdTim <timofeev2001maksim@gmail.com>
2022-03-28 14:13:30 +0300

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>{{about.text}}</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage',
+  data(){
+    return{
+      about: {}
+    }
+  },
+  mounted(){
+    this.fetchAboutInfo()
+  },
+  methods:{
+    async fetchAboutInfo(){
+      this.about = await this.$axios.$get('https://demo-api.vsdev.space/api/elonus/about_page')
+    }
+  }
+}
+</script>
